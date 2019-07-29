ff65d1351b5568fe01652eedc319aa6ffe25d679
Heart <>
2019-07-29 18:16:05 +0200

    pull Heart.Auth from git

--- Heart.Auth/Heart.Auth.Logic/Jwt/IJwtGenerator.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Heart.Auth.Logic.Jwt
-{
-    public interface IJwtGenerator
-    {
-        string GenerateToken(IdentityUser identityUser);
-    }
-}
