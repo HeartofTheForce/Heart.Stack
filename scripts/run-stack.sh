caf6d1ba511328dc46fe62909268cdb41113bbce
Heart <>
2019-07-29 23:40:04 +0200

    add run-stack script

---
+++ scripts/run-stack.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+docker-compose up -d
+
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:5601/api/status)" != "200" ]]; do sleep 5; echo "Waiting for Kibana..."; done
+echo "Setting Index Pattern..."
+curl --request POST \
+  --url http://localhost:5601/api/saved_objects/index-pattern \
+  --header 'content-type: application/json' \
+  --header 'kbn-version: 7.2.0' \
+  --data '{
+	"attributes": {
+		"title": "filebeat-*",
+		"timeFieldName": "@timestamp"
+	}
+}' >/dev/null
+echo "Stack Ready. View logs http://localhost:5601/app/kibana#/discover"
+echo "Press enter to clean up..."
+read -n 1 -s -r -p ""
+docker-compose down
