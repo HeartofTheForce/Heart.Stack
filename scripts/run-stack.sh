a88cf4c6a7903b577531cafc1bc4b44ca9529c95
Heart <>
2019-07-30 16:27:58 +0200

    pull only one branch rebuild with no cache

--- scripts/run-stack.sh
+++ scripts/run-stack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+docker-compose rm
+docker-compose build --no-cache
 docker-compose up -d
-
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:5601/api/status)" != "200" ]]; do sleep 5; echo "Waiting for Kibana..."; done
 echo "Setting Index Pattern..."
 curl --request POST \
@@ -16,4 +17,4 @@ curl --request POST \
 echo -e "\e[32mStack Ready\e[0m. View logs http://localhost:5601/app/kibana#/discover"
 echo "Press enter to clean up..."
 read -n 1 -s -r -p ""
-docker-compose down
+docker-compose down --rmi 'local'
