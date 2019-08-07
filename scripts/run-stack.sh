a4b20b2f59b1c4de48fa6493f30c2f8ce446957b
Heart <>
2019-08-07 23:58:59 +0200

    export/import kibana objects

--- scripts/run-stack.sh
+++ scripts/run-stack.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 docker-compose rm
 docker-compose build --no-cache
-docker-compose up -d --scale tictactoe-client=3
+docker-compose up -d --scale tictactoe-client=6
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:5601/api/status)" != "200" ]]; do sleep 5; echo "Waiting for Kibana..."; done
 echo "Setting Index Pattern..."
-curl --request POST \
-  --url http://localhost:5601/api/saved_objects/index-pattern/618f3ea0-b256-11e9-9c4d-73f7dc7fa305 \
-  --header 'content-type: application/json' \
-  --header 'kbn-version: 7.3.0' \
-  --data '{
-	"attributes": {
-		"title": "filebeat-*",
-		"timeFieldName": "@timestamp"
-	}
-}' >/dev/null
+curl -X POST "localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@config/export.ndjson >/dev/null
 echo -e "\e[32mStack Ready\e[0m. View logs http://localhost:5601/app/kibana#/discover"
 echo "Press enter to clean up..."
 read -n 1 -s -r -p ""
