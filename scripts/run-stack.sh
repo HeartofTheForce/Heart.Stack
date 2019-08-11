7c4f9d99fe651f737f6b333df2fad077af9f359e
Gawaine Du Toit <heartoftheforce@gmail.com>
2019-08-11 10:35:28 +0200

    Feature/make things (#3)
    * export/import kibana objects
    
    * update export
    
    * update dashboard
    
    * point to master
    
    * go
    
    * add 108p dashboard
    
    * less clients
    
    * update exports

--- scripts/run-stack.sh
+++ scripts/run-stack.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-docker-compose rm
+docker-compose rm -s -f
 docker-compose build --no-cache
-docker-compose up -d --scale tictactoe-client=3
+docker-compose up -d --scale tictactoe-client=8
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:5601/api/status)" != "200" ]]; do sleep 5; echo "Waiting for Kibana..."; done
-echo "Setting Index Pattern..."
-curl --request POST \
-  --url http://localhost:5601/api/saved_objects/index-pattern/618f3ea0-b256-11e9-9c4d-73f7dc7fa305 \
-  --header 'content-type: application/json' \
-  --header 'kbn-version: 7.3.0' \
-  --data '{
-	"attributes": {
-		"title": "filebeat-*",
-		"timeFieldName": "@timestamp"
-	}
-}' >/dev/null
-echo -e "\e[32mStack Ready\e[0m. View logs http://localhost:5601/app/kibana#/discover"
+echo -e "\e[32mImporting Kibana Objects\e[0m..."
+curl -X POST "localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@config/export.ndjson >/dev/null
+echo -e "\e[32mView Dashboard\e[0m: http://localhost:5601/app/kibana#/dashboard/6335ded0-b95e-11e9-88b1-030f8b6a784d?_g=()"
+echo -e "\e[32mView Logs\e[0m: http://localhost:5601/app/kibana#/discover"
 echo "Press enter to clean up..."
 read -n 1 -s -r -p ""
 docker-compose down --rmi 'local'
