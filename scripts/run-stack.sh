e287f66118dd4dd5a64a89f29024c29f5886a94b
Heart <>
2019-08-11 10:30:37 +0200

    less clients

--- scripts/run-stack.sh
+++ scripts/run-stack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker-compose rm -s -f
 docker-compose build --no-cache
-docker-compose up -d --scale tictactoe-client=12
+docker-compose up -d --scale tictactoe-client=8
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:5601/api/status)" != "200" ]]; do sleep 5; echo "Waiting for Kibana..."; done
 echo -e "\e[32mImporting Kibana Objects\e[0m..."
 curl -X POST "localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@config/export.ndjson >/dev/null
