08105d46f65d7967c96b07a375f84cd37fbcaebf
Heart <>
2020-07-07 00:58:58 +0200

    initialize elastic stack properly

---
+++ scripts/run-stack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker-compose rm -s -f
+docker-compose build
+docker-compose up -d
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:5601/api/status)" != "200" ]]; do sleep 5; echo "Waiting for Kibana..."; done
+echo -e "\e[32mImporting Kibana Objects\e[0m..."
+curl -X POST "localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@config/export.ndjson >/dev/null
+echo -e "\e[32mView Dashboard\e[0m: http://localhost:5601/app/kibana#/dashboard/6335ded0-b95e-11e9-88b1-030f8b6a784d"
+echo -e "\e[32mView Logs\e[0m: http://localhost:5601/app/kibana#/discover"
+echo "Press enter to clean up..."
+read -n 1 -s -r -p ""
+docker-compose down --rmi 'local'
